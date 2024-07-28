67c77b004d8d1e75c024aadbb2986aeb62762337
neapu <neapu@neapu.com>
2024-07-28 16:17:03 +0800

    更新C++20风格的代码行获取方式

--- logger.cpp
+++ logger.cpp
@@ -1,15 +1,10 @@
 #include "logger.h"
-#include "logger.h"
-#include <cstdio>
-#include <ctime>
 #include <chrono>
-#include <iomanip>
-#include <mutex>
-#include <string>
+#include <iostream>
 #ifdef _WIN32
 #include <io.h>
 #include <direct.h>
-#include <windows.h>
+#include <Windows.h>
 #define access _access
 #define mkdir(x, y) _mkdir(x)
 #else
@@ -21,35 +16,59 @@
 #include <android/log.h>
 #endif
 
-using namespace neapu;
-
-#define ToCString str().c_str
-#define IsEmpty empty
-
-FunctionTracer::FunctionTracer(const char* fileName, const char* funcName) : m_fileName(fileName), m_funcName(funcName)
-{
-    Logger(LM_DEBUG) << "Enter function: " << fileName << ":" << m_funcName;
-}
-
-FunctionTracer::~FunctionTracer()
-{
-    Logger(LM_DEBUG) << "Leave function: " << m_fileName << ":" << m_funcName;
-}
-
-int Logger::m_nLogLevel = LM_NOLOG;
-int Logger::m_nPrintLevel = LM_DEBUG;
+namespace neapu {
+LogLevel Logger::m_printLevel = LogLevel::LM_DEBUG;
+LogLevel Logger::m_logLevel = LogLevel::LM_NOLOG;
+std::string Logger::m_logPath;
+std::string Logger::m_logPrefix;
+std::string Logger::m_logFileName;
 FILE* Logger::m_pFile = nullptr;
-Logger::String Logger::m_strLogDate;
 std::mutex Logger::m_fileMutex;
-Logger::String Logger::m_strLogPath;
 bool Logger::m_firstLog = true;
-Logger::String Logger::m_strLogPrefix;
 
-Logger::Logger(int level) : m_nLevel(level) {}
+void Logger::setPrintLevel(const LogLevel level)
+{
+    m_printLevel = level;
+}
+
+void Logger::setLogLevel(LogLevel level, const std::string& logPath, const std::string& logPrefix)
+{
+    if (logPath.empty()) {
+        m_logLevel = LogLevel::LM_NOLOG;
+        return;
+    }
+    m_logPath = logPath;
+    if (m_logPath.back() != '/' && m_logPath.back() != '\\') {
+        m_logPath += '/';
+    }
+    m_logLevel = level;
+    if (access(m_logPath.c_str(), 0) == -1) {
+        if (mkdir(m_logPath.c_str(), 0744) == -1) {
+            std::cerr << "Failed to create log directory: " << m_logPath << std::endl;
+            m_logLevel = LogLevel::LM_NOLOG;
+            return;
+        }
+    }
+    m_logPrefix = logPrefix;
+}
+
+Logger::Logger(const LogLevel level, const std::source_location& loc)
+    : m_level(level)
+    , m_loc(loc)
+{
+}
 
 Logger::~Logger()
 {
-    std::time_t now_c = time(nullptr);
+    if (m_level <= m_printLevel || m_level <= m_logLevel) {
+        const std::string logText = makeLogString(m_level, m_loc, m_data.str());
+        printLog(m_level, logText);
+        writeLog(m_level, logText);
+    }
+}
+
+std::string Logger::makeLogString(const LogLevel level, const std::source_location& loc, const std::string& logText)
+{
     std::string threadId;
 #ifdef _WIN32
     threadId = std::to_string(GetCurrentThreadId());
@@ -57,138 +76,99 @@ Logger::~Logger()
     pthread_t tid = pthread_self();
     threadId = std::to_string((uint64_t)tid);
 #endif
-    if (m_nLevel <= m_nLogLevel) {
-        if (openFile()) {
-            std::unique_lock<std::mutex> locker(m_fileMutex);
-            if (m_firstLog) {
-                m_firstLog = false;
-                fprintf(m_pFile, "------------------------%s------------------------\n", GetTime().c_str());
-            }
-            fprintf(m_pFile, "%s%s%s\n", GetTime().c_str(), getLevelFlag(m_nLevel, false), m_data.ToCString());
-            fflush(m_pFile);
-        }
+    std::string logLevel;
+    switch (level) {
+    case LogLevel::LM_DEADLY: logLevel = "Deadly";
+        break;
+    case LogLevel::LM_ERROR: logLevel = "Error";
+        break;
+    case LogLevel::LM_WARNING: logLevel = "Warning";
+        break;
+    case LogLevel::LM_INFO: logLevel = "Info";
+        break;
+    case LogLevel::LM_DEBUG: logLevel = "Debug";
+        break;
+    case LogLevel::LM_VERBOSE: logLevel = "Verbose";
+        break;
+    default: logLevel = "Unknown";
+        break;
     }
-    if (m_nLevel <= m_nPrintLevel) {
-#ifdef _WIN32
-        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-        DWORD dwWritten;
-        StringStream rst;
-        rst << GetTime() << getLevelFlag(m_nLevel, true) << m_data.ToCString() << "\r\n";
-        WriteConsoleA(hConsole, rst.ToCString(), rst.str().length(), &dwWritten, nullptr);
-#else
-        fprintf(stderr, "%s%s[%s]%s\n", GetTime().c_str(), getLevelFlag(m_nLevel, false), threadId.c_str(), m_data.ToCString());
-#endif
+    return std::format("[{}][{}][{}][{}:{}][{}]: {}", getTimeString(), logLevel, threadId, loc.file_name(), loc.line(), loc.function_name(),
+                       logText);
+}
+
+std::string Logger::getTimeString()
+{
+    using namespace std::chrono;
+    const auto now = system_clock::now();
+    const auto now_c = system_clock::to_time_t(now);
+    const auto now_ms = duration_cast<milliseconds>(now.time_since_epoch()) % 1000;
+    auto [tm_sec, tm_min, tm_hour, tm_mday, tm_mon, tm_year, tm_wday, tm_yday, tm_isdst] = *std::localtime(&now_c);
+    return std::format("{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}.{:03d}", tm_year + 1900, tm_mon + 1, tm_mday,
+                       tm_hour, tm_min, tm_sec, now_ms.count());
+}
+
+void Logger::printLog(const LogLevel level, const std::string& logText)
+{
+    if (level > m_printLevel) return;
+    std::cout << logText << std::endl;
 #ifdef __ANDROID__
-        switch (m_nLevel) {
-        case LM_DEADLY:
-            __android_log_print(ANDROID_LOG_FATAL, "neapu_log", "%s%s[%s]%s\n", GetTime().c_str(), getLevelFlag(m_nLevel, false), threadId.c_str(), m_data.ToCString());
-            break;
-        case LM_ERROR:
-            __android_log_print(ANDROID_LOG_ERROR, "neapu_log", "%s%s[%s]%s\n", GetTime().c_str(), getLevelFlag(m_nLevel, false), threadId.c_str(), m_data.ToCString());
-            break;
-        case LM_WARNING:
-            __android_log_print(ANDROID_LOG_WARN, "neapu_log", "%s%s[%s]%s\n", GetTime().c_str(), getLevelFlag(m_nLevel, false), threadId.c_str(), m_data.ToCString());
-            break;
-        case LM_INFO:
-            __android_log_print(ANDROID_LOG_INFO, "neapu_log", "%s%s[%s]%s\n", GetTime().c_str(), getLevelFlag(m_nLevel, false), threadId.c_str(), m_data.ToCString());
-            break;
-        case LM_DEBUG:
-            __android_log_print(ANDROID_LOG_DEBUG, "neapu_log", "%s%s[%s]%s\n", GetTime().c_str(), getLevelFlag(m_nLevel, false), threadId.c_str(), m_data.ToCString());
-            break;
-        case LM_VERBOSE:
-            __android_log_print(ANDROID_LOG_VERBOSE, "neapu_log", "%s%s[%s]%s\n", GetTime().c_str(), getLevelFlag(m_nLevel, false), threadId.c_str(), m_data.ToCString());
-            break;
-        }
-#endif
+    switch (m_level) {
+    case LM_DEADLY:
+        __android_log_print(ANDROID_LOG_FATAL, "neapu_log", "%s\n", logText.c_str());
+        break;
+    case LM_ERROR:
+        __android_log_print(ANDROID_LOG_ERROR, "neapu_log", "%s\n", logText.c_str());
+        break;
+    case LM_WARNING:
+        __android_log_print(ANDROID_LOG_WARN, "neapu_log", "%s\n", logText.c_str());
+        break;
+    case LM_INFO:
+        __android_log_print(ANDROID_LOG_INFO, "neapu_log", "%s\n", logText.c_str());
+        break;
+    case LM_DEBUG:
+        __android_log_print(ANDROID_LOG_DEBUG, "neapu_log", "%s\n", logText.c_str());
+        break;
+    case LM_VERBOSE:
+        __android_log_print(ANDROID_LOG_VERBOSE, "neapu_log", "%s\n", logText.c_str());
+        break;
     }
+#endif
 }
 
-void Logger::setLogLevel(int nLogLevel, const String& strLogPath, const String& strLogPrefix)
+void Logger::writeLog(const LogLevel level, const std::string& logText)
 {
-    if (strLogPath.IsEmpty()) return;
-    m_strLogPath = strLogPath;
-    if (m_strLogPath.back() != '/') {
-        m_strLogPath.push_back('/');
-    }
-    const char* path = m_strLogPath.c_str();
-    if (0 != access(path, 0)) {
-        if (0 != mkdir(path, 0744)) {
-            perror("mkdir");
-            return;
-        }
-    }
-    m_nLogLevel = nLogLevel;
-    m_strLogPrefix = strLogPrefix;
-}
+    if (level > m_logLevel) return;
+    if (m_logPath.empty()) return;
 
-void Logger::setPrintLevel(int nPrintLevel)
-{
-    m_nPrintLevel = nPrintLevel;
+    if (!openFile()) {
+        std::cerr << "Failed to open log file: " << m_logPath << std::endl;
+        return;
+    }
+
+    std::lock_guard<std::mutex> lock(m_fileMutex);
+    if (m_firstLog) {
+        m_firstLog = false;
+        fprintf(m_pFile, "------------------------%s------------------------\n", getTimeString().c_str());
+    }
+    fprintf(m_pFile, "%s\n", logText.c_str());
+    fflush(m_pFile);
 }
-#ifdef _WIN32
-void neapu::Logger::setConsoleChcp()
-{
-    system("chcp 65001");
-}
-#endif
 
 bool Logger::openFile()
 {
     std::time_t now_c = time(nullptr);
     char temp[64];
     strftime(temp, 64, "%Y%m%d", localtime(&now_c));
-    if (m_pFile) {
-        if (m_strLogDate == temp) return true;
-    }
-    m_strLogDate = temp;
     char szNewFile[128];
-    snprintf(szNewFile, 128, "%s%s_%s.log", m_strLogPath.c_str(), m_strLogPrefix.c_str(), temp);
-    if (m_pFile) fclose(m_pFile);
+    snprintf(szNewFile, 128, "%s%s_%s.log", m_logPath.c_str(), m_logPrefix.c_str(), temp);
+    if (m_pFile) {
+        if (m_logFileName == szNewFile) return true;
+        fclose(m_pFile);
+    }
+    m_logFileName = szNewFile;
     m_pFile = fopen(szNewFile, "a");
     if (!m_pFile) return false;
     return true;
 }
-
-const char* Logger::getLevelFlag(int level, bool bColor)
-{
-    if (bColor) {
-        switch (level) {
-        case LM_DEADLY: return "\033[0;35;40m[Deadly]\033[0m";
-        case LM_ERROR: return "\033[0;31;40m[Error]\033[0m";
-        case LM_WARNING: return "\033[0;33;40m[Warning]\033[0m";
-        case LM_INFO: return "\033[0;32;40m[Info]\033[0m";
-        case LM_DEBUG: return "\033[0;34;40m[Debug]\033[0m";
-        case LM_VERBOSE: return "\033[0;36;40m[Verbose]\033[0m";
-        }
-    } else {
-        switch (level) {
-        case LM_DEADLY: return "[Deadly]";
-        case LM_ERROR: return "[Error]";
-        case LM_WARNING: return "[Warning]";
-        case LM_INFO: return "[Info]";
-        case LM_DEBUG: return "[Debug]";
-        case LM_VERBOSE: return "[Verbose]";
-        }
-    }
-
-    return "[Unknow]";
-}
-
-Logger::String Logger::GetTime()
-{
-    using namespace std::chrono;
-    std::time_t tt = system_clock::to_time_t(system_clock::now());
-    auto microsecond = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count() % 1000000;
-
-#ifdef _WIN32
-    struct std::tm valtm;
-    localtime_s(&valtm, &tt);
-    auto pValtm = &valtm;
-#else
-    auto pValtm = localtime(&tt);
-#endif
-
-    std::stringstream ss;
-    ss << "[" << std::put_time(pValtm, "%F %X") << " " << std::setw(3) << std::setfill('0') << microsecond / 1000 << "]";
-    return ss.str();
 }
--- logger.h
+++ logger.h
@@ -1,57 +1,29 @@
-#ifndef __LOGGER_H__
-#define __LOGGER_H__
-#include <sstream>
+#ifndef LOGGER_H_
+#define LOGGER_H_
 #include <string>
+#include <format>
+#include <source_location>
+#include <sstream>
 #include <mutex>
-#include <string.h>
-#define LM_NOLOG 0
-#define LM_DEADLY 1
-#define LM_ERROR 2
-#define LM_WARNING 3
-#define LM_INFO 4
-#define LM_DEBUG 5
-#define LM_VERBOSE 6
-
-#ifdef _WIN32
-#define __FILENAME__ (strrchr(__FILE__, '\\') ? (strrchr(__FILE__, '\\') + 1) : __FILE__)
-#else
-#define __FILENAME__ (strrchr(__FILE__, '/') ? (strrchr(__FILE__, '/') + 1) : __FILE__)
-#endif
-
-#define LOG(x) neapu::Logger(x) << "[" << __FILENAME__ << ":" << __LINE__ << "][" << __FUNCTION__ << "]"
-#define LOG_DEBUG LOG(LM_DEBUG)
-#define LOG_INFO LOG(LM_INFO)
-#define LOG_WARNING LOG(LM_WARNING)
-#define LOG_ERROR LOG(LM_ERROR)
-#define LOG_DEADLY LOG(LM_DEADLY)
-#define LOG_VERBOSE LOG(LM_VERBOSE)
-
-#define FUNC_TRACE neapu::FunctionTracer __tracer__(__FILENAME__, __FUNCTION__)
 
 namespace neapu {
-class FunctionTracer {
-public:
-    FunctionTracer(const char* fileName, const char* funcName);
-    ~FunctionTracer();
-
-private:
-    const char* m_fileName;
-    const char* m_funcName;
+enum LogLevel {
+    LM_NOLOG = 0,
+    LM_DEADLY = 1,
+    LM_ERROR = 2,
+    LM_WARNING = 3,
+    LM_INFO = 4,
+    LM_DEBUG = 5,
+    LM_VERBOSE = 6
 };
 
-class Logger {
-    using String = std::string;
-    using StringStream = std::stringstream;
-
+class Logger final {
 public:
-    Logger(int level);
-    ~Logger();
+    static void setPrintLevel(LogLevel level);
+    static void setLogLevel(LogLevel level, const std::string& logPath, const std::string& logPrefix = "");
 
-    static void setLogLevel(int nLogLevel, const String& strLogPath, const String& strLogPrefix = "");
-    static void setPrintLevel(int nPrintLevel);
-#ifdef _WIN32
-    static void setConsoleChcp();
-#endif
+    explicit Logger(LogLevel level, const std::source_location& loc = std::source_location::current());
+    ~Logger();
 
     template <class T>
     Logger& operator<<(T&& t)
@@ -60,24 +32,75 @@ public:
         return *this;
     }
 
+    template <class... Args>
+    Logger& format(std::format_string<Args...> fmt, Args&&... args)
+    {
+        m_data << std::format(fmt, std::forward<Args>(args)...);
+        return *this;
+    }
+
+    template <class... Args>
+    static Logger& deadly(const std::source_location& loc, std::format_string<Args...> fmt, Args&&... args)
+    {
+        return Logger(LogLevel::LM_DEADLY, loc).format(fmt, std::forward<Args>(args)...);
+    }
+#define deadly(...) deadly(std::source_location::current() , __VA_ARGS__)
+
+    template <class... Args>
+    static Logger& error(const std::source_location& loc, std::format_string<Args...> fmt, Args&&... args)
+    {
+        return Logger(LogLevel::LM_ERROR, loc).format(fmt, std::forward<Args>(args)...);
+    }
+#define error(...) error(std::source_location::current() , __VA_ARGS__)
+
+    template <class... Args>
+    static Logger& warning(const std::source_location& loc, std::format_string<Args...> fmt, Args&&... args)
+    {
+        return Logger(LogLevel::LM_WARNING, loc).format(fmt, std::forward<Args>(args)...);
+    }
+#define warning(...) warning(std::source_location::current() , __VA_ARGS__)
+
+    template <class... Args>
+    static Logger& info(const std::source_location& loc, std::format_string<Args...> fmt, Args&&... args)
+    {
+        return Logger(LogLevel::LM_INFO, loc).format(fmt, std::forward<Args>(args)...);
+    }
+#define info(...) info(std::source_location::current() , __VA_ARGS__)
+
+    template <class... Args>
+    static Logger& debug(const std::source_location& loc, std::format_string<Args...> fmt, Args&&... args)
+    {
+        return Logger(LogLevel::LM_DEBUG, loc).format(fmt, std::forward<Args>(args)...);
+    }
+#define debug(...) debug(std::source_location::current() , __VA_ARGS__)
+
+    template <class... Args>
+    static Logger& verbose(const std::source_location& loc, std::format_string<Args...> fmt, Args&&... args)
+    {
+        return Logger(LogLevel::LM_VERBOSE, loc).format(fmt, std::forward<Args>(args)...);
+    }
+#define verbose(...) verbose(std::source_location::current() , __VA_ARGS__)
+
 private:
+    static std::string makeLogString(LogLevel level, const std::source_location& loc, const std::string& logText);
+    static std::string getTimeString();
+    static void printLog(LogLevel level, const std::string& logText);
+    static void writeLog(LogLevel level, const std::string& logText);
     static bool openFile();
-    static const char* getLevelFlag(int level, bool bColor);
-    static String GetTime();
 
 private:
-    int m_nLevel;
-    StringStream m_data;
-
-    static int m_nLogLevel;
-    static int m_nPrintLevel;
+    LogLevel m_level;
+    std::stringstream m_data;
+    std::source_location m_loc;
+    static LogLevel m_printLevel;
+    static LogLevel m_logLevel;
+    static std::string m_logPath;
+    static std::string m_logPrefix;
+    static std::string m_logFileName;
     static FILE* m_pFile;
-    static String m_strLogDate;
     static std::mutex m_fileMutex;
-    static String m_strLogPath;
     static bool m_firstLog;
-    static String m_strLogPrefix;
 };
 } // namespace neapu
 
-#endif // __LOGGER_H__
+#endif // LOGGER_H_
